2aefb4c6740cd239489b182bdb44ba47ad6ae3a0
lizuzheng <491288234@qq.com>
2017-01-19 14:43:31 +0800

    新电脑测试提交

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //测试
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
